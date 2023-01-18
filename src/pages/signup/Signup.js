c18939a9cb07a4f95428a9304865e6746a98ccd2
Mayanja Andrew <andremayanja@gmail.com>
2023-01-18 06:21:03 +0300

    setup

---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,9 @@
+import styles from "./Signup.module.css";
+
+import React from "react";
+import Home from "../home/Home";
+import Login from "../login/Login";
+
+export default function Signup() {
+  return <div>dsd</div>;
+}
