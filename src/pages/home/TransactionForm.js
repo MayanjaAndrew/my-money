a6cb71c446cd5aa0b9337e0b09cebcabbc396290
Mayanja Andrew <andremayanja@gmail.com>
2023-02-17 15:42:30 +0300

    final comps

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions");
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
+    addDocument({
+      uid,
       name,
       amount,
     });
   };
+  // reset the form fields
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
   return (
     <>
       <h3>Add a transaction</h3>
